875758be700a571a05d48f298d3ce57e929df4bc
danila.dru <danila.dru@gmail.com>
2022-05-02 10:43:39 +0300

    katas 02/05/2022

---
+++ src/LoopInspector.java
@@ -0,0 +1,19 @@
+//import java.util.*;
+public class LoopInspector {
+
+//    public int loopSize(Node node) {
+//        HashMap<Node, Integer> map = new HashMap<Node, Integer>();
+//        boolean flag = true;
+//        int count = 0;
+//        while (flag) {
+//            Node nextNode = node.getNext();
+//            count++;
+//            if (map.get(nextNode) == null) {
+//                map.put(nextNode, count);
+//            } else {return count - map.get(nextNode);}
+//
+//            node = nextNode;
+//        }
+//        return 0;
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -66,7 +66,10 @@ public class Main {
 //        Solution.arrayToTree(new Int[] {17, 0, -4, 3, 15});
 
 //        https://www.codewars.com/kata/55c6126177c9441a570000cc/train/java
-        System.out.println(WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+//        System.out.println(WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+
+
+
 
     }
 }
